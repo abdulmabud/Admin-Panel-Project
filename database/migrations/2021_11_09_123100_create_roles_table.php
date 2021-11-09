2518c51310a175e9db4c736738fbbd863403419f
Abdul Mabud <abdulmabudm@gmail.com>
2021-11-09 18:33:54 +0600

    Role model and migration added

---
+++ database/migrations/2021_11_09_123100_create_roles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('slug', 50)->unique();
+            $table->string('note', 250)->nullable();
+            $table->tinyInteger('deleteable')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
